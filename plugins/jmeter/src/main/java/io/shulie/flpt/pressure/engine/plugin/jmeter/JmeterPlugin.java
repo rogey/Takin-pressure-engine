baeb9108d51009c4a346c69ebdbde5ef241b6410
liangxiaorong <>
2021-09-29 17:01:59 +0800

    修改场景ID和reportId传值

--- plugins/jmeter/src/main/java/io/shulie/flpt/pressure/engine/plugin/jmeter/JmeterPlugin.java
+++ plugins/jmeter/src/main/java/io/shulie/flpt/pressure/engine/plugin/jmeter/JmeterPlugin.java
@@ -291,8 +291,8 @@ public class JmeterPlugin implements PressurePlugin {
             "-j", jmeterLogFilePath, "-Duser.timezone=Asia/Shanghai", "-Djava.net.preferIPv4Stack=true"
                 , "-Djava.net.preferIPv4Addresses=true"
                 , "-Dengine.perssure.mode=" + context.getCurrentEnginePressureMode().getCode(),
-            "-Dpod.number" , podNum, "-DSceneId=" + sceneId, "-DReportId=" + reportId
-            , "-DCustomerId=" + customerId, "-DCallbackUrl=" + context.getCloudCallbackUrl()
+            "-Dpod.number" , podNum, "-DSceneId" , sceneId, "-DReportId" , reportId
+            , "-DCustomerId" , customerId, "-DCallbackUrl=" + context.getCloudCallbackUrl()
             , "-DSamplingInterval=" + traceSampling, portRule};
         //组装后端监听器参数
         args = metricArgsProcess(context, args);
